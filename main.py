c0a3495cf72666165c0018ed53db0e7cdbe5c00f
saurabh choudhary <mentalpirate5@gmail.com>
2025-10-05 06:43:16 +0000

    added flask app

--- main.py
+++ main.py
@@ -5,5 +5,8 @@ app = Flask(__name__)
 def index():
     return render_template('maintenance.html')
 
+@app.route("/error")
+def error():
+    return "Error Page"
 if __name__ == '__main__':
     app.run()
